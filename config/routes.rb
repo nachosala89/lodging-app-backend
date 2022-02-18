881455f1876b6a02a855ecbcc483aa4c3eb3a31a
LOctavio <luis_ram1997@hotmail.com>
2022-02-18 14:12:45 -0600

    Add api endpoints for reservations

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
      resources :rooms, only: [:index, :show, :create, :destroy]
+     resources :reservations, only: [:index, :show, :create]
     end
   end
 end
