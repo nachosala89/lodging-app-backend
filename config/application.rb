448b30fb89c0d606f6c9036813c1c0ab90c6a31e
LOctavio <luis_ram1997@hotmail.com>
2022-02-18 13:09:21 -0600

    Add rack-cors gem and add config

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,11 @@ module LodgingAppBackend
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
