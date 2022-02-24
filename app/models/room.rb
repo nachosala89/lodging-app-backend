e88d9506e0f43adc92dd27ca7687de8c57f0dddd
LOctavio <luis_ram1997@hotmail.com>
2022-02-24 14:22:02 -0600

    Validates check out and check in dates

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,16 @@ class Room < ApplicationRecord
   belongs_to :hotel
   has_many :reservations
   has_many :users, through: :reservations
+
+  def unavailable_dates
+    all_dates = []
+    reservations.pluck(:check_in_date, :check_out_date).map do |dates|
+      current_date = dates[0]
+      while current_date <= dates[1]
+        all_dates.push(current_date)
+        current_date += 1.day
+      end
+    end
+    all_dates
+  end
 end
