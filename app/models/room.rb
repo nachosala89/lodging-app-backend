b3925ee24fd704a9c91cff1fb0ddbbf1b5b8cc11
LOctavio <luis_ram1997@hotmail.com>
2022-02-28 09:55:16 -0600

    Add validations for room

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,6 +3,10 @@ class Room < ApplicationRecord
   has_many :reservations
   has_many :users, through: :reservations
 
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :people_amount, presence: true, numericality: { greater_than: 0 }
+
   def unavailable_dates
     all_dates = []
     reservations.pluck(:check_in_date, :check_out_date).map do |dates|
